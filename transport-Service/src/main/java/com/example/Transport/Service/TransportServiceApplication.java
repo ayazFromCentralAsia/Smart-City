72cb188e413b38a76d83af2dd896e32a4cb80930
ayazFromCentralAsia <wiliamraartur@gmail.com>
2025-03-01 18:38:51 +0600

    Add new service

---
+++ transport-Service/src/main/java/com/example/Transport/Service/TransportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Transport.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportServiceApplication.class, args);
+	}
+
+}
