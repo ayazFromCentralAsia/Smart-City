316510be4e3eb709476470a9423e3a94f60db467
ayazFromCentralAsia <wiliamraartur@gmail.com>
2025-03-02 22:08:31 +0600

    Add gateway

--- auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.OPENIDCONNECT,
 		scheme = "bearer", bearerFormat = "JWT")
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
